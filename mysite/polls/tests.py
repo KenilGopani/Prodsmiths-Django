d2f63097eded2aaff02c8ca910f11a0d9d6cf8e3
KenilGopani <kenilgopani600@gmail.com>
2023-10-18 14:18:30 +0530

    Unit testing using baker lib.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -2,48 +2,50 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
-from .models import Question
 
-#Model testing
+from .models import Question, Choice
+from model_bakery import baker
+
+# Model testing
 class QuestionModelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #Generating instance of Question model using baker
+        cls.future_question = baker.make('polls.Question', pub_date=timezone.now() + datetime.timedelta(days=30))
+        cls.old_question = baker.make('polls.Question', pub_date=timezone.now() - datetime.timedelta(days=1, seconds=1))
+        cls.recent_question = baker.make('polls.Question', pub_date=timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59))
+    
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+        self.assertIs(self.future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
         is older than 1 day.
         """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
+        self.assertIs(self.old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
         """
         was_published_recently() returns True for questions whose pub_date
         is within the last day.
         """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+        self.assertIs(self.recent_question.was_published_recently(), True)
+
+#Choice test using baker library 
+
+class ChoiceModelTests(TestCase):
+    def test_fake_test_to_create_choice_instance(slef):
+        choice = baker.make('polls.Choice', _quantity=3)
+        print("Here is the list of question in choice instance")
+        # print(choice.question.question_text)
 
 
-def create_question(question_text, days):
-    """
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
-
-#View Testing 
+# View Testing
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
@@ -59,7 +61,8 @@ class QuestionIndexViewTests(TestCase):
         Questions with a pub_date in the past are displayed on the
         index page.
         """
-        question = create_question(question_text="Past question.", days=-30)
+        time = timezone.now() + datetime.timedelta(days=-30)
+        question = baker.make('polls.Question', pub_date=time)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(
             response.context["latest_question_list"],
@@ -71,7 +74,8 @@ class QuestionIndexViewTests(TestCase):
         Questions with a pub_date in the future aren't displayed on
         the index page.
         """
-        create_question(question_text="Future question.", days=30)
+        time = timezone.now() + datetime.timedelta(days=30)
+        question = baker.make('polls.Question', pub_date=time)
         response = self.client.get(reverse("polls:index"))
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
@@ -81,8 +85,13 @@ class QuestionIndexViewTests(TestCase):
         Even if both past and future questions exist, only past questions
         are displayed.
         """
-        question = create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future question.", days=30)
+        #past question
+        time = timezone.now() + datetime.timedelta(days=-30)
+        question = baker.make('polls.Question', pub_date=time)
+
+        #future question
+        time = timezone.now() + datetime.timedelta(days=30)
+        baker.make('polls.Question', pub_date=time)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(
             response.context["latest_question_list"],
@@ -93,13 +102,15 @@ class QuestionIndexViewTests(TestCase):
         """
         The questions index page may display multiple questions.
         """
-        question1 = create_question(question_text="Past question 1.", days=-30)
-        question2 = create_question(question_text="Past question 2.", days=-5)
+        question1 = baker.make('polls.Question', pub_date=timezone.now() + datetime.timedelta(days=-30))
+        question2 = baker.make('polls.Question', pub_date=timezone.now() + datetime.timedelta(days=-5))
+
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(
             response.context["latest_question_list"],
             [question2, question1],
         )
+
 
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
@@ -107,7 +118,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the future
         returns a 404 not found.
         """
-        future_question = create_question(question_text="Future question.", days=5)
+        future_question = baker.make('polls.Question', pub_date=timezone.now() + datetime.timedelta(days=5))
         url = reverse("polls:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -117,7 +128,8 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-        past_question = create_question(question_text="Past Question.", days=-5)
+        past_question = baker.make('polls.Question', pub_date=timezone.now() + datetime.timedelta(days=-5))
+        # print(past_question.id)
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
